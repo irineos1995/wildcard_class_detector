4dd56da67ec21972ea2da12dd45b299f98cbc83f
Eirinaios Theodorou <irineos1995@gmail.com>
2020-06-16 10:38:23 +0100

    functionality to get parents of each class in a dictionary added

--- detector.py
+++ detector.py
@@ -79,4 +79,42 @@ def get_wildcard_classes(source_code_file):
     childless_classes = set(get_childless_classes(source_code_file))
     return parentless_classes.intersection(childless_classes)
 
-print(get_wildcard_classes('test_cases/case1.html'))
+
+def get_parents_recursively_(tree, class_parents_dict):
+    if not tree:
+        return class_parents_dict
+    else:
+        if "childGenerator" in dir(tree):
+            for child in tree.childGenerator():
+                if isinstance(child, Tag):
+                    child_class = tuple(child.get('class', ()))
+                    if child_class:
+                        if child_class not in class_parents_dict:
+                            if child.parent and child.parent.get('class', ()):
+                                class_parents_dict[child_class] = [tuple(child.parent.get('class', ()))]
+                        else:
+                            if child.parent and child.parent.get('class', ()):
+                                class_parents_dict[child_class].append(tuple(child.parent.get('class', ())))
+                else:
+                    continue
+                get_parents_recursively_(child, class_parents_dict)
+        else:
+            if not tree.isspace(): #Just to avoid printing "\n" parsed from document.
+                pass
+    return class_parents_dict
+
+def get_parents_of_each_class(source_code_file):
+    '''
+        This function is responsible for identifying CSS classes' DIRECT parent.
+    '''
+    class_parents_dict = {}
+    with open(source_code_file, 'r') as source_code:
+        parsed_code = BeautifulSoup(source_code, 'html.parser')
+        for child in parsed_code.childGenerator():
+            if isinstance(child, Tag):
+                class_parents_dict = get_parents_recursively_(child, class_parents_dict={})
+    return class_parents_dict
+                
+
+# print(get_wildcard_classes('test_cases/case1.html'))
+print(get_parents_of_each_class('test_cases/case1.html'))
